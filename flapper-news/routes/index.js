568e1fc2549e2a5239c96c480121d4f3809944e8
matthewlevy <matthew.levy@gmail.com>
2015-10-05 05:01:09 +0000

    added method for upvoting to comments schema

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -77,7 +77,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
 
 router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   req.comment.upvote(function(err, comment) {
-    if (err) { return next(err); }
+    if (err) { return next(err);}
     res.json(comment);
   });
 });
